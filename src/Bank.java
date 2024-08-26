d7a4bf46d94247b8b4ac8556b3474e023cca550d
tirivashee <tirivashee@gmail.com>
2024-08-26 22:13:26 +0200

    Question 2: Add deposit method to Bank class

--- src/Bank.java
+++ src/Bank.java
@@ -3,4 +3,10 @@ class Bank {
     // Fields for accountType and accountBalance
     private String accountType;
     private double accountBalance;
+
+    // Method to deposit an amount and return the updated balance
+    public double deposit(double amount) {
+        accountBalance += amount;
+        return accountBalance;
+    }
 }
